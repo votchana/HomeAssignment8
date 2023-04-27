adcd1a8da802825b2fefd35c62cfaf823b8598e9
votchana <130603756+votchana@users.noreply.github.com>
2023-04-27 14:21:42 -0300

    Delete ThisChangeUniqueForLianaBranch.java

--- src/main/java/ThisChangeUniqueForLianaBranch.java
+++
@@ -1,7 +0,0 @@
-public class ThisChangeUniqueForLianaBranch {
-    public static void main(String[] args){
-        System.out.println("Hi. I made this changes for Liana's branch while in Another branch ");
-        return;
-    }
-
-}
