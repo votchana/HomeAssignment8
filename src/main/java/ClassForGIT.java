7ddbc1cafdcfbc18673be5e78cad303a09e40f7e
Liana Votchal <liana.votchal@iqvia.com>
2023-04-27 13:51:55 -0300

    Edited 1 class after created my own branch

--- src/main/java/ClassForGIT.java
+++ src/main/java/ClassForGIT.java
@@ -2,6 +2,7 @@ public class ClassForGIT {
     public static void main(String[] args){
         System.out.println("Hi. This is for initial commit");
         System.out.println("This is to my created branch");
+        System.out.println("This is to my created branch");
         return;
     }
 }
